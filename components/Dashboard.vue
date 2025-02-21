6c638d6d293c9d69adfd0e3cbb5f8e670f1977a5
w3WebTech <164163622+w3WebTech@users.noreply.github.com>
2025-02-21 12:55:28 +0530

    fix

---
+++ components/Dashboard.vue
@@ -0,0 +1,184 @@
+<template>
+    <div class="card flex justify-center">
+        <Stepper value="1" class="basis-[50rem]">
+            <StepList>
+                <Step value="1">Step 1</Step>
+                <Step value="2">Step 2</Step>
+                <Step value="3">Step 3</Step>
+            </StepList>
+            <StepPanels>
+                <StepPanel v-slot="{ activateCallback }" value="1">
+                    <div class="flex flex-col h-full">
+                        <div class="border-2 border-dashed border-surface-200 dark:border-surface-700 rounded bg-surface-50 dark:bg-surface-950 flex-auto flex justify-center items-center font-medium">
+                            <div class="card flex justify-center py-10">
+                                <div class="flex flex-col gap-2">
+                                    <label for="username">Username</label>
+                                    <InputText id="username" v-model="Username" aria-describedby="username-help" />
+                              
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex pt-6 justify-end">
+                        <Button label="Next" icon="pi pi-arrow-right" iconPos="right" @click="validateStep1(activateCallback)" />
+                    </div>
+                </StepPanel>
+
+                <StepPanel v-slot="{ activateCallback }" value="2">
+                    <div class="flex flex-col h-full">
+                        <div class="border-2 border-dashed border-surface-200 dark:border-surface-700 rounded bg-surface-50 dark:bg-surface-950 flex-auto flex justify-center items-center font-medium">
+                            <div class="card py-10">
+                                <Toast />
+                                <FileUpload name="demo[]" url="/api/upload" @upload="onTemplatedUpload" :multiple="true" accept="image/*" :maxFileSize="1000000" @select="onSelectedFiles">
+                                    <template #header="{ chooseCallback, uploadCallback, clearCallback, files }">
+                                        <div class="gap-">
+                                            <div class="flex gap-2">
+                                                <Button @click="chooseCallback()" icon="pi pi-images" rounded outlined severity="secondary"></Button>
+                                            </div>
+                                        </div>
+                                    </template>
+                                    <template #content="{ files, uploadedFiles, removeUploadedFileCallback, removeFileCallback }">
+                                        <div class="flex flex-col gap-4 pt-1">
+                                            <div v-if="files.length > 0">
+                                                <h5>Files to Upload</h5>
+                                                <div class="flex flex-wrap gap-2">
+                                                    <div v-for="(file, index) of files" :key="file.name + file.type + file.size" class="p-8 rounded-border flex flex-col border border-surface items-center gap-4">
+                                                        <div>
+                                                            <img role="presentation" :alt="file.name" :src="file.objectURL" width="70" height="30" />
+                                                        </div>
+                                                        <span class="font-semibold text-ellipsis max-w-60 whitespace-nowrap overflow-hidden">{{ file.name }}</span>
+                                                        <div>{{ formatSize(file.size) }}</div>
+                                                        <Button icon="pi pi-times" @click="onRemoveTemplatingFile(file, removeFileCallback, index)" outlined rounded severity="danger" />
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div v-if="uploadedFiles.length > 0">
+                                                <h5>Completed</h5>
+                                                <div class="flex flex-wrap gap-2">
+                                                    <div v-for="(file, index) of uploadedFiles" :key="file.name + file.type + file.size" class="p-8 rounded-border flex flex-col border border-surface items-center gap-4">
+                                                        <div>
+                                                            <img role="presentation" :alt="file.name" :src="file.objectURL" width="100" height="50" />
+                                                        </div>
+                                                        <span class="font-semibold text-ellipsis max-w-60 whitespace-nowrap overflow-hidden">{{ file.name }}</span>
+                                                        <div>{{ formatSize(file.size) }}</div>
+                                                        <Badge value="Completed" class="mt-4" severity="success" />
+                                                        <Button icon="pi pi-times" @click="removeUploadedFileCallback(index)" outlined rounded severity="danger" />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </template>
+                                    <template #empty>
+                                        <div class="flex items-center justify-center flex-col">
+                                            <i class="pi pi-cloud-upload !border-2 !rounded-full !p-5 !text-4xl !text-muted-color" />
+                                            <p class="mt-6 mb-0">Drag and drop files to here to upload.</p>
+                                        </div>
+                                    </template>
+                                </FileUpload>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex pt-6 justify-between">
+                        <Button label="Back" severity="secondary" icon="pi pi-arrow-left" @click="activateCallback('1')" />
+                        <Button label="Next" icon="pi pi-arrow-right" iconPos="right" @click="validateStep2(activateCallback)" />
+                    </div>
+                </StepPanel>
+
+                <StepPanel v-slot="{ activateCallback }" value="3">
+                    <div class="flex flex-col h-full">
+                        <div class="border-2 border-dashed border-surface-200 dark:border-surface-700 rounded bg-surface-50 dark:bg-surface-950 flex-auto flex justify-center items-center font-medium">
+                            <div class="card flex justify-center py-10">
+                                <div class="flex flex-col gap-2">
+                                    <label for="username">ADDRESS</label>
+                                    <Textarea v-model="value" rows="5" cols="30" />
+                                
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pt-6">
+                        <Button label="Back" severity="secondary" icon="pi pi-arrow-left" @click="activateCallback('2')" />
+                    </div>
+                </StepPanel>
+            </StepPanels>
+        </Stepper>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { usePrimeVue } from 'primevue/config';
+import { useToast } from 'primevue/usetoast';
+
+const Username = ref(null);
+const address = ref(null);
+const files = ref([]);
+const uploadedFiles = ref([]);
+const totalSize = ref(0);
+const totalSizePercent = ref(0);
+const $primevue = usePrimeVue();
+const toast = useToast();
+
+const formatSize = (bytes) => {
+    const k = 1024;
+    const dm = 3;
+    const sizes = $primevue.config.locale.fileSizeTypes;
+
+    if (bytes === 0) {
+        return `0 ${sizes[0]}`;
+    }
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    const formattedSize = parseFloat((bytes / Math.pow(k, i)).toFixed(dm));
+
+    return `${formattedSize} ${sizes[i]}`;
+};
+
+// Step 1 Validation
+const validateStep1 = (activateCallback) => {
+    if (!Username.value) {
+        toast.add({ severity: 'error', summary: 'Error', detail: 'Please enter a username.', life: 3000 });
+    } else {
+        activateCallback('2');
+    }
+};
+
+// Step 2 Validation
+const validateStep2 = (activateCallback) => {
+    // Check if files are empty after removing any files
+    if (files.value.length === 0) {
+        toast.add({ severity: 'error', summary: 'Error', detail: 'Please upload at least one file.', life: 3000 });
+    } else {
+        activateCallback('3');
+    }
+};
+
+// File Upload Methods
+const onRemoveTemplatingFile = (file, removeFileCallback, index) => {
+    removeFileCallback(index);
+    // Manually re-check files after removal
+    if (files.value.length === 0) {
+        toast.add({ severity: 'error', summary: 'Error', detail: 'Please upload at least one file.', life: 3000 });
+    }
+    files.value=[];
+    totalSize.value -= parseInt(formatSize(file.size));
+    totalSizePercent.value = totalSize.value / 10;
+};
+
+const onSelectedFiles = (event) => {
+    files.value = event.files.map((file) => ({
+        ...file,
+    }));
+    files.value.forEach((file) => {
+        totalSize.value += parseInt(formatSize(file.size));
+    });
+};
+
+const onTemplatedUpload = () => {
+    uploadedFiles.value = [...uploadedFiles.value, ...files.value];
+    files.value = [];
+    toast.add({ severity: 'info', summary: 'Success', detail: 'Files uploaded', life: 3000 });
+};
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,213 @@
+
+<template>
+    <div class="min-h-screen flex relative lg:static bg-surface-50 dark:bg-surface-950">
+        <div id="app-sidebar-1"
+            class="bg-surface-0 dark:bg-surface-950 h-screen hidden lg:block flex-shrink-0 absolute lg:static left-0 top-0 z-10 border-r border-surface select-none w-[280px]">
+            <div class="flex flex-col h-full">
+                <div class="flex items-center p-4 flex-shrink-0 h-[60px]">
+                  <img src="@/public/images.jpg" alt="">
+                </div>
+                <div class="overflow-y-auto">
+                    <ul class="list-none p-4 m-0 bg-white">
+                        <li>
+                            <div v-styleclass="{
+                                selector: '@next',
+                                enterFromClass: 'hidden',
+                                enterActiveClass: 'animate-slidedown',
+                                leaveToClass: 'hidden',
+                                leaveActiveClass: 'animate-slideup'
+                            }"
+                                class="p-4 flex items-center justify-between text-surface-600 dark:text-surface-200 cursor-pointer">
+                                <span class="font-medium">FAVORITES</span>
+                                <i class="pi pi-chevron-down" />
+                            </div>
+                            <ul class="list-none p-0 m-0 overflow-hidden">
+                                <li>
+                                    <a @click="setSelectedItem('dashboard')"
+                                        class="flex items-center cursor-pointer p-4 rounded-border text-surface-700 dark:text-surface-100 hover:bg-surface-100 dark:hover:bg-surface-700 duration-150 transition-colors">
+                                        <i class="pi pi-home mr-2 !text-xl !leading-none" />
+                                        <span class="font-medium">Dashboard</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a @click="setSelectedItem('bookmarks')"
+                                        class="flex items-center cursor-pointer p-4 rounded-border text-surface-700 dark:text-surface-100 hover:bg-surface-100 dark:hover:bg-surface-700 duration-150 transition-colors">
+                                        <i class="pi pi-bookmark mr-2 !text-xl !leading-none" />
+                                        <span class="font-medium">Bookmarks</span>
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a
+                                        class="flex items-center cursor-pointer p-4 rounded-border text-surface-700 dark:text-surface-100 hover:bg-surface-100 dark:hover:bg-surface-700 duration-150 transition-colors">
+                                        <i class="pi pi-users mr-2 !text-xl !leading-none" />
+                                        <span class="font-medium">Team</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        class="flex items-center cursor-pointer p-4 rounded-border text-surface-700 dark:text-surface-100 hover:bg-surface-100 dark:hover:bg-surface-700 duration-150 transition-colors">
+                                        <i class="pi pi-comments mr-2 !text-xl !leading-none" />
+                                        <span class="font-medium">Messages</span>
+                                        <span
+                                            class="inline-flex items-center justify-center ml-auto bg-primary text-primary-contrast dark:text-surface-900 rounded-full min-w-6 h-6">3</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        class="flex items-center cursor-pointer p-4 rounded-border text-surface-700 dark:text-surface-100 hover:bg-surface-100 dark:hover:bg-surface-700 duration-150 transition-colors">
+                                        <i class="pi pi-calendar mr-2 !text-xl !leading-none" />
+                                        <span class="font-medium">Calendar</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        class="flex items-center cursor-pointer p-4 rounded-border text-surface-700 dark:text-surface-100 hover:bg-surface-100 dark:hover:bg-surface-700 duration-150 transition-colors">
+                                        <i class="pi pi-cog mr-2 !text-xl !leading-none" />
+                                        <span class="font-medium">Settings</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul class="list-none p-4 m-0">
+                        <li>
+                            <div v-styleclass="{
+                                selector: '@next',
+                                enterFromClass: 'hidden',
+                                enterActiveClass: 'animate-slidedown',
+                                leaveToClass: 'hidden',
+                                leaveActiveClass: 'animate-slideup'
+                            }"
+                                class="p-4 flex items-center justify-between text-surface-600 dark:text-surface-200 cursor-pointer">
+                                <span class="font-medium">APPLICATION</span>
+                                <i class="pi pi-chevron-down" />
+                            </div>
+                            <ul class="list-none p-0 m-0 overflow-hidden">
+                                <li>
+                                    <a
+                                        class="flex items-center cursor-pointer p-4 rounded-border text-surface-700 dark:text-surface-100 hover:bg-surface-100 dark:hover:bg-surface-700 duration-150 transition-colors">
+                                        <i class="pi pi-folder mr-2 !text-xl !leading-none" />
+                                        <span class="font-medium">Projects</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        class="flex items-center cursor-pointer p-4 rounded-border text-surface-700 dark:text-surface-100 hover:bg-surface-100 dark:hover:bg-surface-700 duration-150 transition-colors">
+                                        <i class="pi pi-chart-bar mr-2 !text-xl !leading-none" />
+                                        <span class="font-medium">Performance</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        class="flex items-center cursor-pointer p-4 rounded-border text-surface-700 dark:text-surface-100 hover:bg-surface-100 dark:hover:bg-surface-700 duration-150 transition-colors">
+                                        <i class="pi pi-cog mr-2 !text-xl !leading-none" />
+                                        <span class="font-medium">Settings</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div class="mt-auto">
+                    <hr class="mb-4 mx-4 border-t border-0 border-surface" />
+                    <a
+                        class="m-4 flex items-center cursor-pointer p-4 rounded-border text-surface-700 dark:text-surface-100 hover:bg-surface-100 dark:hover:bg-surface-700 duration-150 transition-colors">
+                        <img src="https://fqjltiegiezfetthbags.supabase.co/storage/v1/render/image/public/block.images/blocks/avatars/circle/avatar-f-1.png"
+                            class="mr-2 w-8 h-7" />
+                        <span class="font-medium">Amy Elsner</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="min-h-screen flex flex-col relative flex-auto">
+            <div
+                class="flex justify-between items-center px-8 bg-surface-0 dark:bg-surface-900 border-b border-surface relative lg:static h-[60px]">
+                <div class="flex">
+                    <a v-styleclass="{
+                        selector: '#app-sidebar-1',
+                        enterFromClass: 'hidden',
+                        enterActiveClass: 'animate-fadeinleft',
+                        leaveToClass: 'hidden',
+                        leaveActiveClass: 'animate-fadeoutleft',
+                        hideOnOutsideClick: true
+                    }" class="cursor-pointer block lg:hidden text-surface-700 dark:text-surface-100 mr-4 mt-1">
+                        <i class="pi pi-bars text-4xl" />
+                    </a>
+                    <IconField icon-position="left">
+                        <InputIcon class="pi pi-search text-surface-700 dark:text-surface-100" />
+                        <InputText type="text" class="border-0 w-40 sm:w-80" placeholder="Search" />
+                    </IconField>
+                </div>
+                <a v-styleclass="{
+                    selector: '@next',
+                    enterFromClass: 'hidden',
+                    enterActiveClass: 'animate-fadein',
+                    leaveToClass: 'hidden',
+                    leaveActiveClass: 'animate-fadeout',
+                    hideOnOutsideClick: true
+                }" class="cursor-pointer block lg:hidden text-surface-700 dark:text-surface-100">
+                    <i class="pi pi-ellipsis-v text-2xl" />
+                </a>
+                <ul
+                    class="list-none p-0 m-0 hidden lg:flex lg:items-center select-none lg:flex-row border lg:border-0 border-surface right-0 top-full z-10 shadow lg:shadow-none absolute lg:static bg-surface-0 dark:bg-surface-900">
+                    <li>
+                        <a
+                            class="flex p-4 lg:px-4 lg:py-2 items-center text-surface-600 dark:text-surface-200 hover:text-surface-900 dark:hover:text-surface-0 hover:bg-surface-100 dark:hover:bg-surface-700 font-medium rounded-border cursor-pointer duration-150 transition-colors">
+                            <i class="pi pi-inbox text-base lg:!text-2xl leading-none mr-2 lg:mr-0" />
+                            <span class="block lg:hidden font-medium">Inbox</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            class="flex p-4 lg:px-4 lg:py-2 items-center text-surface-600 dark:text-surface-200 hover:text-surface-900 dark:hover:text-surface-0 hover:bg-surface-100 dark:hover:bg-surface-700 font-medium rounded-border cursor-pointer duration-150 transition-colors">
+                            <OverlayBadge severity="danger" class="mr-2 lg:mr-0">
+                                <i class="pi pi-bell text-base lg:!text-2xl leading-none" />
+                            </OverlayBadge>
+                            <span class="block lg:hidden font-medium">Notifications</span>
+                        </a>
+                    </li>
+                    <li class="border-t border-surface lg:border-t-0">
+                        <a
+                            class="flex p-4 lg:px-4 lg:py-2 items-center hover:bg-surface-100 dark:hover:bg-surface-700 font-medium rounded-border cursor-pointer duration-150 transition-colors">
+                            <img src="https://fqjltiegiezfetthbags.supabase.co/storage/v1/render/image/public/block.images/blocks/avatars/circle/avatar-f-1.png"
+                                class="mr-4 lg:mr-0 w-8 h-8" />
+                            <div class="block lg:hidden">
+                                <div class="text-surface-900 dark:text-surface-0 font-medium">Josephine Lillard</div>
+                                <span class="text-surface-600 dark:text-surface-200 font-medium text-sm">Marketing
+                                    Specialist</span>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="p-8 flex flex-col flex-auto">
+                <div
+                    class=" my-10 bg-surface-0 dark:bg-surface-950 flex-auto">
+                    <div v-if="selectedItem === 'dashboard'">
+                        <Dashboard />
+                    </div>
+                    <div v-if="selectedItem === 'bookmarks'">2 value</div>
+                    <div v-if="selectedItem === 'reports'">3 value</div>
+                    <div v-if="selectedItem === 'team'">4 value</div>
+                    <div v-if="selectedItem === 'messages'">5 value</div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+<script setup>
+import IconField from 'primevue/iconfield';
+import InputIcon from 'primevue/inputicon';
+import InputText from 'primevue/inputtext';
+import OverlayBadge from 'primevue/overlaybadge';
+import 'primeicons/primeicons.css';
+import Dashboard from './components/Dashboard.vue';
+const selectedItem = ref('dashboard');
+
+
+const setSelectedItem = (item) => {
+    selectedItem.value = item;
+};
+</script>
